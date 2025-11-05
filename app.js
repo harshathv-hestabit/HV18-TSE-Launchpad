3f0890e9d9e2c2b97d2d3481ac2ea54ce96b1be3
Harshath Venkatesh <harshath.venkatesh@hestabit.in>
2025-11-05 16:54:02 +0530

    commit 6

--- app.js
+++ app.js
@@ -3,3 +3,4 @@ console.log(Day 3 at hestabit);
 console.log(task on git advanced commands);
 console,log(bisect,revert,stash,cherry pick);
 console.log(currently doing task 1);
+console.log(creating 8 commits);
